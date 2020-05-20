ae25e05dd8c1d78e8586b1229b1763c6ff432603
Mario Gabriell <mariogk01@gmail.com>
2020-05-20 16:56:09 -0300

    Adding configuration watcher
    Removing SaveAll feature
    Using System.Text.Json
    Adding configuration watcher

--- EasyConfig/ConfigurationManager.cs
+++ EasyConfig/ConfigurationManager.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace EasyConfig
 {
@@ -11,11 +11,6 @@ namespace EasyConfig
         /// Determines which folder will be used to save the configuration files. Default = Configs.
         /// </summary>
         public static string SavePath { get; set; } = "Configs";
-
-        /// <summary>
-        /// This list is to just store the references of the configs gotten by the Get function to be used by the SaveAll to be saved when called.
-        /// </summary>
-        private static List<object> _configsFetched = new List<object>();
 
         /// <summary>
         /// Returns the saved configuration file.
@@ -35,9 +30,8 @@ namespace EasyConfig
                 try
                 {
                     var defaultClass = Activator.CreateInstance<T>();
-                    var json = JsonConvert.SerializeObject(defaultClass);
+                    var json = JsonSerializer.Serialize(defaultClass);
                     File.WriteAllText(filePath, json);
-                    _configsFetched.Add(defaultClass);
                     return defaultClass;
                 }
                 catch(Exception e)
@@ -51,25 +45,13 @@ namespace EasyConfig
             try
             {
                 var fileContents = File.ReadAllText(filePath);
-                var configObject = JsonConvert.DeserializeObject<T>(fileContents);
-                _configsFetched.Add(configObject);
+                var configObject = JsonSerializer.Deserialize<T>(fileContents);
                 return configObject;
             }
             catch(Exception e)
             {
                 e.Data.Add("ConfigTestError", $"Failed to fetch the configuration file with the name {name}.");
                 throw;
-            }
-        }
-
-        /// <summary>
-        /// Saves all configuration gotten using this class.
-        /// </summary>
-        public static void SaveAll()
-        {
-            foreach (var config in _configsFetched)
-            {
-                Save(config);
             }
         }
 
@@ -88,7 +70,7 @@ namespace EasyConfig
             {
                 var filePath = Path.Combine(SavePath, $"{name}.json");
             
-                var json = JsonConvert.SerializeObject(config);
+                var json = JsonSerializer.Serialize(config);
                 File.WriteAllText(filePath, json);
             }
             catch(Exception e)
---
+++ EasyConfig/ConfigurationWatcher.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace EasyConfig
+{
+    public class ConfigurationWatcher<T>
+    {
+        public bool AutoStart { get; set; }
+
+        private FileSystemWatcher _fileSystemWatcher;
+
+        public ConfigurationWatcher()
+        {
+            if (AutoStart)
+            {
+                Start();
+            }
+        }
+
+        public void Start()
+        {
+            _fileSystemWatcher = new FileSystemWatcher
+            {
+                Filter = ""
+                
+            };
+            
+            _fileSystemWatcher.Changed += FileSystemWatcherOnChanged;
+
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            _fileSystemWatcher.EnableRaisingEvents = false;
+            
+            var configContents = File.ReadAllText(e.FullPath);
+            var config = JsonSerializer.Deserialize<T>(configContents);
+            ConfigChanged?.Invoke(config);
+            
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        public void Stop()
+        {
+            if (_fileSystemWatcher != null)
+            {
+                _fileSystemWatcher.EnableRaisingEvents = false;
+                _fileSystemWatcher.Dispose();
+                _fileSystemWatcher = null;
+            }
+        }
+
+        public delegate void OnConfigChangedDelegate(T config);
+
+        public event OnConfigChangedDelegate ConfigChanged;
+    }
+}
